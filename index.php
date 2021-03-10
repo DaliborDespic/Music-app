9c5dd76f55dde9eec1706937ee7a609fc3abc328
DaliborDespic <you@example.com>
2021-03-10 20:35:43 +0100

    header html

--- index.php
+++ index.php
@@ -1,5 +1,15 @@
 <?php include "partials/header.php"; ?>
 
-    <h1 style="font-size: 9rem; text-align:center;">Home</h1>
+  <!-- header -->
+  <header class="mainHeader">
+      <div>
+        <h1><span>Music</span>for everyone</h1>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptates nesciunt repellendus reiciendis iste, mollitia alias? Ipsa ab quaerat harum, omnis placeat nam delectus, magnam facilis aliquam alias, dicta expedita? Molestias.</p>
+        <button>Start free trial</button>
+      </div>
+      <div>
+        <img src="./img/header.png" alt="">
+      </div>
+  </header>
 
 <?php include "partials/footer.php"; ?>
